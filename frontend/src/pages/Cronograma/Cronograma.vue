b74395078d8b43d7ceda0bf7814e43c417505543
saaghha <david.tapia@alumnos.usm.cl>
2021-06-18 21:44:42 -0400

    cambios en plan minero

--- frontend/src/pages/Cronograma/Cronograma.vue
+++ frontend/src/pages/Cronograma/Cronograma.vue
@@ -42,7 +42,7 @@
                       <b-td>
                         <b-button-group>
                           <b-button variant="warning" class="btn-sm" @click="selectItem(item)">Editar</b-button>
-                          <b-button variant="danger" class="btn-sm">Eliminar</b-button>
+                          <b-button variant="danger" class="btn-sm" @click="confirmDelete(item.slug)">Eliminar</b-button>
                         </b-button-group>
                       </b-td>
                     </b-tr>
@@ -85,7 +85,7 @@
                       <b-td>
                         <b-button-group>
                           <b-button variant="warning" class="btn-sm" @click="selectItem(item)">Editar</b-button>
-                          <b-button variant="danger" class="btn-sm">Eliminar</b-button>
+                          <b-button variant="danger" class="btn-sm" @click="confirmDelete(item.slug)">Eliminar</b-button>
                         </b-button-group>
                       </b-td>
                     </b-tr>
@@ -97,14 +97,35 @@
           <b-tab title="Producción" @click="selectTab('producción')">
             <div class="row">
               <div class="col-lg-12 col-md-12 col-sm-12">
-                <b-table :items="produccion" :fields="fields_prod" responsive small hover no-border-collapse striped>
-                  <template #cell(options)="row">
-                    <b-button-group>
-                      <b-button variant="warning" class="btn-sm">Editar</b-button>
-                      <b-button variant="danger" class="btn-sm">Eliminar</b-button>
-                    </b-button-group>
-                  </template>
-                </b-table>
+                <b-table-simple hover small caption-top responsive>
+                  <b-thead>
+                    <b-tr>
+                      <b-th sticky-column>Nombre Producción</b-th>
+                      <b-th v-for="(anio, index_field) in anos_produccion" :key="index_field">
+                        <div class="pointer" @click="selectValue(anio.key)">
+                          {{anio.label}}
+                        </div>
+                        </b-th>
+                      <b-th>Total</b-th>
+                      <b-th>Opciones</b-th>
+                    </b-tr>
+                  </b-thead>
+                  <b-tbody>
+                    <b-tr v-for="(item, index_item) in produccion" :key="index_item">
+                      <b-td>{{item.nombre}}</b-td>
+                      <b-td class="text-center" v-for="(value_ano, index_ano) in anos_produccion" :key="index_ano">
+                        {{mostrarValor(value_ano, item)}}
+                      </b-td>
+                      <b-td>{{formatAllMoney(item.total_desgloce_total)}}</b-td>
+                      <b-td>
+                        <b-button-group>
+                          <b-button variant="warning" class="btn-sm" @click="selectItem(item)">Editar</b-button>
+                          <b-button variant="danger" class="btn-sm" @click="confirmDelete(item.slug)">Eliminar</b-button>
+                        </b-button-group>
+                      </b-td>
+                    </b-tr>
+                  </b-tbody>
+                </b-table-simple>
               </div>
             </div>
           </b-tab>
@@ -113,6 +134,7 @@
     </div>
     <NewItemCronograma v-if="showNewItemModal" :showModal="showNewItemModal" :id_datos_mina="this.$store.getters.slugDatosMina" @close="handleNewModal(false)" @add="getCronograma()"/>
     <NewPreparacionCronograma v-if="showNewPrepaModal" :showModal="showNewPrepaModal" :id_datos_mina="this.$store.getters.slugDatosMina" @close="handleNewModal(false)" @add="getCronograma()"/>
+    <NewProduccionCronograma v-if="showNewProdModal" :showModal="showNewProdModal" :id_datos_mina="this.$store.getters.slugDatosMina" @close="handleNewModal(false)" @add="getCronograma()"/>
     <EditItemCronograma
       v-if="editInfraItemModal"
       :showModal="editInfraItemModal"
@@ -123,6 +145,13 @@
     <EditPreparacionCronograma
       v-if="editPrepaItemModal"
       :showModal="editPrepaItemModal"
+      :id_dato="selectedEdit.slug"
+      :id_datos_mina="this.$store.getters.slugDatosMina"
+      @close="handleEditModal(false)"
+      @edit="getCronograma()"/>
+    <EditProduccionCronograma
+      v-if="editProdItemModal"
+      :showModal="editProdItemModal"
       :id_dato="selectedEdit.slug"
       :id_datos_mina="this.$store.getters.slugDatosMina"
       @close="handleEditModal(false)"
@@ -138,7 +167,9 @@
 import NewItemCronograma from './New'
 import EditItemCronograma from './EditInfraestructura'
 import EditPreparacionCronograma from './EditPreparacionModal.vue'
+import EditProduccionCronograma from './EditProduccionModal.vue'
 import NewPreparacionCronograma from './NewPreparacion.vue'
+import NewProduccionCronograma from './NewProduccionModal.vue'
 import helpers from '../../components/Helper'
 import CronogramaPeriodo from './CronogramaPeriodo'
 export default {
@@ -148,15 +179,19 @@ export default {
     EditItemCronograma,
     CronogramaPeriodo,
     NewPreparacionCronograma,
-    EditPreparacionCronograma
+    NewProduccionCronograma,
+    EditPreparacionCronograma,
+    EditProduccionCronograma
   },
   data () {
     return {
       datos: [],
       showNewItemModal: false,
       showNewPrepaModal: false,
+      showNewProdModal: false,
       editInfraItemModal: false,
       editPrepaItemModal: false,
+      editProdItemModal: false,
       datos_final: [
         {total: 2330003}
       ],
@@ -269,11 +304,14 @@ export default {
       if(!cond) {
         this.editInfraItemModal = cond
         this.editPrepaItemModal = cond
+        this.editProdItemModal = cond
       }
       if(tab == 'infraestructura') {
         this.editInfraItemModal = cond
       } else if(tab == 'preparación') {
         this.editPrepaItemModal = cond
+      } else  if(tab == 'producción') {
+        this.editProdItemModal = cond
       }
     },
     selectValue: function (anio) {
@@ -308,15 +346,51 @@ export default {
       this.selectedEdit = JSON.parse(JSON.stringify(item))
       this.handleEditModal(true, this.tab_selected)
     },
+    deleteItem: function (tab, slug) {
+      var url = 'cronograma/'
+      if(tab == 'infraestructura') {
+        url = url + 'infraestructura/'+slug
+      } else if (tab == 'preparación') {
+        url = url + 'preparacion/'+slug
+      } else if (tab == 'producción') {
+        url = url + 'produccion/'+slug
+      }
+      this.$store.commit('setLoading', true)
+      axios.delete(url).then((response) => {
+        this.showToast({icon: 'success', title: 'Item eliminado correctamente'})
+        this.getCronograma()
+      }).catch((err) => {
+        this.showToast({icon: 'error', title: err.response.data.message})
+        this.$store.commit('setLoading', false)
+      })
+    },
+    confirmDelete: function (slug) {
+      this.$swal({
+        title: '¿Seguro?',
+        icon: 'question',
+        text: '¿Estás seguro de eliminar el item?',
+        confirmButtonText: 'Eliminar',
+        showCancelButton: true,
+        confirmButtonColor: '#dc3545',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if(result.value) {
+          this.deleteItem(this.tab_selected, slug)
+        }
+      })
+    },
     handleNewModal: function (cond, tab) {
       if(!cond) {
         this.showNewItemModal = cond
         this.showNewPrepaModal = cond
+        this.showNewProdModal = cond
       }
       if(tab == 'infraestructura') {
         this.showNewItemModal = cond
       } else if (tab == 'preparación') {
         this.showNewPrepaModal = cond
+      } else if (tab == 'producción') {
+        this.showNewProdModal = cond
       }
     }
   }
--- frontend/src/pages/Cronograma/CronogramaPeriodo.vue
+++ frontend/src/pages/Cronograma/CronogramaPeriodo.vue
@@ -38,14 +38,60 @@
       <b-tab title="Preparación">
         <div class="row">
           <div class="col-lg-12 col-md-12 col-sm-12">
-            
+            <b-table-simple hover small caption-top responsive>
+              <b-thead>
+                <b-tr>
+                  <b-th sticky-column>Nombre Preparación</b-th>
+                  <b-th sticky-column>Área</b-th>
+                  <b-th sticky-column>Sección</b-th>
+                  <b-th sticky-column>N° Tiros</b-th>
+                  <b-th sticky-column>Metros Totales</b-th>
+                  <b-th v-for="(periodo, index_field) in data.periodo_preparacion" :key="index_field">
+                    {{periodo.label}}
+                  </b-th>
+                  <b-th>Total</b-th>
+                </b-tr>
+              </b-thead>
+              <b-tbody>
+                <b-tr v-for="(item, index_item) in data.preparacion" :key="index_item">
+                  <b-td>{{item.nombre}}</b-td >
+                  <b-td>{{item.area}}</b-td>
+                  <b-td>{{item.seccion}}</b-td>
+                  <b-td>{{item.nro_tiros}}</b-td>
+                  <b-td>{{item.longitud}}</b-td>
+                  <b-td class="text-center" v-for="(value_ano, index_ano) in data.periodo_preparacion" :key="index_ano">
+                    {{mostrarValor(value_ano, item)}}
+                  </b-td>
+                  <b-td>{{formatAllMoney(item.total_desgloce_total)}}</b-td>
+                </b-tr>
+              </b-tbody>
+            </b-table-simple>
           </div>
         </div>
       </b-tab>
       <b-tab title="Producción">
         <div class="row">
           <div class="col-lg-12 col-md-12 col-sm-12">
-            
+            <b-table-simple hover small caption-top responsive>
+              <b-thead>
+                <b-tr>
+                  <b-th sticky-column>Nombre Producción</b-th>
+                  <b-th v-for="(periodo, index_field) in data.periodo_produccion" :key="index_field">
+                    {{periodo.label}}
+                  </b-th>
+                  <b-th>Total</b-th>
+                </b-tr>
+              </b-thead>
+              <b-tbody>
+                <b-tr v-for="(item, index_item) in data.produccion" :key="index_item">
+                  <b-td>{{item.nombre}}</b-td >
+                  <b-td class="text-left" v-for="(value_ano, index_ano) in data.periodo_produccion" :key="index_ano">
+                    {{mostrarValor(value_ano, item)}}
+                  </b-td>
+                  <b-td>{{formatAllMoney(item.total_desgloce_total)}}</b-td>
+                </b-tr>
+              </b-tbody>
+            </b-table-simple>
           </div>
         </div>
       </b-tab>
