1ba9b85c51f8d15dc09d0bea6f8c387c6d522f27
MauricioArosMorales <mauricioaros.trabajos@gmail.com>
2021-12-13 15:02:33 -0300

    arreglos de plan minero y cronogramas p2

--- backend/app/Http/Controllers/Cronograma/CronogramaInfraestructuraPeriodoController.php
+++ backend/app/Http/Controllers/Cronograma/CronogramaInfraestructuraPeriodoController.php
@@ -622,8 +622,8 @@ class CronogramaInfraestructuraPeriodoController extends Controller
         }
 
         return [
-            'infraestructura' => $data_plan,
-            'periodo_infraestructura' => collect($periodo_infra)->sortBy('key')->toArray(),
+            'infraestructura' => $data_prep,
+            'periodo_infraestructura' => collect($periodo_prep)->sortBy('key')->toArray(),
             'preparacion' => $data_plan_prep,
             'periodo_preparaciones' => collect($periodo_prep)->sortBy('key')->toArray(),
             'produccion' => $data_plan_prod,
